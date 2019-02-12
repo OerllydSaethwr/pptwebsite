75dfaeff7b668f870e3ea63546ef234c1572c10d
David Mark Valaczkai <DMV18@ic.ac.uk>
2019-02-12 21:00:04 +0000

    added drawing on play.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,48 @@
-function replace() {
-    document.getElementById("play-img").setAttribute("hidden");
-    document.getElementById("play-video").removeAttribute("hidden");
+var canvas, context;
+
+window.onload = function () {
+    canvas = document.getElementById("imageView");
+    context = canvas.getContext("2d");
+    var img = document.getElementById("firstFrame");
+    context.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
 }
+
+
+if(window.addEventListener) {
+    window.addEventListener('load', function () {
+        function init() {
+            canvas = document.getElementById("imageView");
+            context = canvas.getContext("2d");
+
+            canvas.addEventListener('mousemove', ev_mousemove, false);
+
+        }
+
+        var started = false;
+
+        function ev_mousemove(ev) {
+            var x, y;
+            var rect = canvas.getBoundingClientRect();
+
+
+
+            if (ev.layerX || ev.layerX == 0) { // Firefox
+                var scaleX = canvas.width / rect.width;
+                var scaleY = canvas.height / rect.height;
+                x = (ev.clientX - rect.left) * scaleX;  //45
+                y = (ev.clientY - rect.top) * scaleY;  //20
+
+            }
+            if (!started) {
+                context.beginPath();
+                context.moveTo(x, y);
+                started = true;
+            } else {
+                context.lineTo(x, y);
+                context.strokeStyle = "blue";
+                context.stroke();
+            }
+        }
+
+        init();
+    }, false);}
